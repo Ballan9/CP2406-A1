a90f231e6d38364c22b954c278db357bdcc579e6
Ballan9 <allan.serrurier@my.jcu.edu.au>
2017-11-03 13:13:06 +1000

    Complete ClientSend Add Server class

--- UserStories/ClientSend.java
+++ UserStories/ClientSend.java
@@ -1,2 +1,14 @@
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+
 public class ClientSend {
+
+    public static void sendMessage(String message)throws Exception{
+        DatagramSocket socket = new DatagramSocket(6987);
+        InetAddress address = InetAddress.getByName("10.999.99.999");
+        DatagramPacket packet = new DatagramPacket(message.getBytes(), message.length(),address,99999);
+        socket.send(packet);
+        socket.close();
+    }
 }
